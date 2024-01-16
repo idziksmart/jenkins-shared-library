223e41ddfbd34030f87740c0cf173e4267111e0c
Tom Idzik <idziksmart@gmail.com>
2024-01-16 12:21:25 +0100

    JSL-3-added-env

--- vars/genericPipeline.groovy
+++ vars/genericPipeline.groovy
@@ -1,17 +1,22 @@
 def call(body) {
     echo "Generic pipeline"
     def ctx = [:]
+    echo "Starting compute method"
     ctx = compute(body)
+    echo "Finished compute method"
 }
 
 def compute(body) {
     def ctx = [:]
     ctx.logger = new io.idziksmart.jenkinssharedlibrary.Logger(this)
-    print(env)
+    print("REGION "+env.AWS_REGION)
+    print("SLACK_CHANNEL "+env.SLACK_CHANNEL)
     print(currentBuild)
+    print(env)
+    print(${env.JENKINS_HOME})
     ctx.env = env
     ctx.currentBuild = currentBuild
-    echo "$ctx.currentBuild"
-    ctx.logger.printMessage('Starting .... ')
-    ctx.logger.printMessage(ctx.currentBuild)
+//    echo "$ctx.currentBuild"
+//    ctx.logger.printMessage('Starting .... ')
+//    ctx.logger.printMessage(ctx.currentBuild)
 }
