d47533ef756c0d1d4eb2925ba360de6dbacfa478
Tom Idzik <idziksmart@gmail.com>
2024-01-16 11:34:14 +0100

    JSL-1-added-Logger

--- vars/genericPipeline.groovy
+++ vars/genericPipeline.groovy
@@ -1,3 +1,14 @@
 def call(body) {
     echo "Generic pipeline"
+    def ctx = [:]
+    ctx = compute(body)
+}
+
+def compute(body) {
+    def ctx = [:]
+    ctx.logger = new io.idziksmart.jenkinssharedlibrary.Logger(this)
+    ctx.env = env
+    ctx.currentBuild = currentBuild
+    ctx.logger.printMessage('Starting .... ')
+    ctx.logger.printMessage(ctx.currentBuild)
 }
